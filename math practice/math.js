f6fa98647e70c13d7752aa139d275cbbbd366b55
adrian soliz <Asoliz241@west-mec.org>
2020-01-26 14:17:09 -0800

    styled my website and fixed some of the functions

--- math practice/math.js
+++ math practice/math.js
@@ -20,23 +20,23 @@ function largeNum(){
      
     // finding the largest number
     if(first === large){
-        output.innerHTML = "first number is the largest number";
+        output.innerHTML =  first +   " number is the largest number";
     }
     if( large < second){
         large = second;
-        output.innerHTML = "second number is the largest number"
+        output.innerHTML = second  + " number is the largest number"
     } if( large < third){
         large = third;
-        output.innerHTML = "third number is the largest number";
+        output.innerHTML = third  + " number is the largest number";
     } if(large < fourth){
         large = fourth;
-        output.innerHTML = "fourth number is the largest number";
+        output.innerHTML = fourth + "number is the largest number";
     } if(large < fifth){
         large = fifth;
-        output.innerHTML = " fifth number is the largest number";
+        output.innerHTML = fifth + " number is the largest number";
     } if( large < sixth){
         large = sixth;
-        output.innerHTML = "sixth number is the largest number";
+        output.innerHTML =   sixth + " number is the largest number";
     } 
     return large;
 }
@@ -53,23 +53,23 @@ function smallNum(){
      
     //finding the smallest number
     if(first === smallest){
-        output.innerHTML = "first number is the smallest number";
+        output.innerHTML =    first +   " number is the smallest number";
     }
     if( smallest > second){
         smallest = second;
-        output.innerHTML = "second number is the smallest number"
+        output.innerHTML = second + " number is the smallest number"
     } if( smallest > third){
         smallest = third;
-        output.innerHTML = "third number is the smallest number";
+        output.innerHTML = third + " number is the smallest number";
     } if(smallest > fourth){
         smallest = fourth;
-        output.innerHTML = "fourth number is the smallest number";
+        output.innerHTML =   fourth + " number is the smallest number";
     } if(smallest > fifth){
         smallest = fifth;
-        output.innerHTML = " fifth number is the smallest number";
+        output.innerHTML =   fifth + " number is the smallest number";
     } if( smallest > sixth){
         smallest = sixth;
-        output.innerHTML = "sixth number is the smallest number";
+        output.innerHTML =   sixth +  "number is the smallest number";
     } 
     return smallest;
 }
@@ -96,7 +96,7 @@ function rang(){
     var rang = largeNum() - smallNum();
     output.innerHTML = rang + " is the range";
 }
-
+//making a function that puts numbers in order
 function order(){
     var first = parseFloat(doc1.value);
     var second = parseFloat(doc2.value);
@@ -112,6 +112,7 @@ function order(){
     var a5 = 0 ;
     var a6 = 0; 
 
+    //finding the min and and removing from the order
     a1 = Math.min(first,second,third,fourth,fifth,sixth);
 
     if(a1 === first){
@@ -256,12 +257,12 @@ function order(){
     }else if(a6 = fifth){
         fifth === sixth;
     }
-
+    //telling how to put it in 
     output.innerHTML = a1 + "," + a2 + "," + a3 + "," + a4 + "," + a5 + "," + a6;
     
 }
 
-
+//finding the middle number and median
 function mid(){
     var first = parseFloat(doc1.value);
     var second = parseFloat(doc2.value);
@@ -277,7 +278,7 @@ function mid(){
     var a3 = 0; 
     var a4 = 0;
    
-
+//puting numbers in order but only using 4 variables
     a1 = Math.min(first,second,third,fourth,fifth,sixth);
 
     if(a1 === first){
@@ -373,10 +374,14 @@ function mid(){
     }else if(a4 === fifth){
         fifth = sixth;
     }
+
+    //writing the output of the answer
     output.innerHTML = a3 + " and " + a4 + " are the middle numbers. " + (a3 + a4) / 2 + " is the median";
 
 }
 
+
+//making a function to find any duplicates
 function dup(){
     var first = parseFloat(doc1.value);
     var second = parseFloat(doc2.value);
@@ -385,12 +390,73 @@ function dup(){
     var fifth = parseFloat(doc5.value);
     var sixth = parseFloat(doc6.value);
 
+    var b1 = 1;
+    var b2 = 1;
+    var b3 = 1; 
+    var b4 = 1;
+    var b5 = 1;
+    var b6 = 1;
 
+
+    //storing number 
+    if(first === second){
+        b1 ++;
+    }if(first === third){
+        b1 ++;
+    }if(first === fourth){
+        b1 ++;
+    }if(first === fifth){
+        b1 ++;
+    }if(first === sixth){
+        b1 ++;
+    }if(second === third){
+        b2 ++;
+    }if(second === fourth){
+        b2 ++;
+    }if(second === fifth){
+        b2 ++;
+    }if(second === sixth){
+        b2 ++;
+    }if(third === fourth){
+        b3 ++;
+    }if(third === fifth){
+        b3 ++;
+    }if(third === sixth){
+        b3 ++;
+    }if(fourth === fifth){
+        b4 ++;
+    }if(fourth === sixth){
+        b4 ++;
+    }if(fifth = sixth){
+        b5 ++;
+    }
+
+    if(b1 === 1 && b2 === 1 && b3 === 1 && b4 === 1 && b5 === 1 && b6 === 1){
+        output.innerHTML += "there is no duplicates"
+    }
+    if(b1 !== 1){
+        output.innerHTML += " " + first + " is a duplicate and it appears " + b1 + " times. ";
+    }
+    if(b2 !== 1 && second !== first){
+        output.innerHTML += " " + second + " is a duplicate and it appears " + b2 + " times. ";
+    }
+    if(b3 !== 1 && third !== second && third !== first){
+        output.innerHTML += " " + third + " is a duplicate and it appears " + b3 + " times. ";
+    }
+    if(b4 !== 1 && fourth !== third && fourth !== second && fourth !== first){
+        output.innerHTML += " " + fourth + " is a duplicate and it appears " + b4 + " times. ";
+    }
+    if(b5 !== 1 && fifth !== fourth && fifth !== third && fifth !== second && fifth !== first){
+        output.innerHTML += " " + fifth + " is a duplicate and it appears " + b5 + " times. ";
+    }
+    if(b6 !== 1 && sixth !== fifth && sixth !== fourth && sixth !== third && sixth !== second && sixth !== first){
+        output.innerHTML += " " + sixth + " is a duplicate and it appears " + b6 + " times. ";
+    }
 
 }
 
 
-
+//finding the number that repeats the most
 function mode(){
     var first = parseFloat(doc1.value);
     var second = parseFloat(doc2.value);
@@ -438,27 +504,35 @@ function mode(){
         b5 ++;
     }
 
-    var max = Math.max(b1,b2,b3,b4,b5,b6);
+    var max = Math.max(b1, b2, b3, b4, b5, b6)
 
-    if(b1 === 1 && b2 === 1 && b3 === 1 && b4 === 1 && b5 === 1 && b6 ===1 ){
-        output.innerHTML = "there are no same number";
-    }else if(max === b1){
-        output.innerHTML = first + " is the mode and  it appears " + b1 + " times";
-    }else if(max === b2){
-        output.innerHTML = second + " is the mode and  it appears " + b2 + " times";
-    }else if(max === b3){
-        output.innerHTML = third + " is the mode and  it appears " + b3 + " times";
-    }else if(max === b4){
-        output.innerHTML = fourth + " is the mode and  it appears " + b4 + " times";
-    }else if(max === b5){
-        output.innerHTML = fifth + " is the mode and  it appears " + b5 + " times";
+    if(b1 === 1 && b2 === 1 && b3 === 1 && b4 === 1 && b5 === 1 && b6 === 1){
+        output.innerHTML += "there is no mode"
+    }
+    if(max === b1){
+        output.innerHTML += " " + first + " is the mode and it appears " + b1 + " times. ";
+    }
+    else if(max === b2){
+        output.innerHTML += " " + second + " is the mode and it appears " + b2 + " times. ";
+    }
+    else if(max === b3){
+        output.innerHTML += " " + third + " is the mode and it appears " + b3 + " times. ";
+    }
+    else if(max === b4){
+        output.innerHTML += " " + fourth + " is the mode and it appears " + b4 + " times. ";
+    }
+    else if(max === b5){
+        output.innerHTML += " " + fifth + " is the mode and it appears " + b5 + " times. ";
+    }else if(max === b6){
+        output.innerHTML += " " + fifth + " is the mode and it appears " + b6 + " times. ";
     }
 
+    
 
 }
 
 
-
+//unique function
 function uni(){
     var first = parseFloat(doc1.value);
     var second = parseFloat(doc2.value);
@@ -467,6 +541,29 @@ function uni(){
     var fifth = parseFloat(doc5.value);
     var sixth = parseFloat(doc6.value);
 
+    
+    output.innerHTML = "";
+    if(first !== second && first !== third && first !== fourth && first !== fifth && first !== sixth){
+        output.innerHTML += " " + first; 
+    }
+    if (second !== first && second !== third && second !== fourth && second !== fifth && second !== sixth){
+        output.innerHTML += " " + second; 
+    }
+    if (third !== first && third !== second && third !== fourth && third !== fifth && third !== sixth){
+        output.innerHTML += " " + third; 
+    }
+    if (fourth !== first && fourth !== second && fourth !== third && fourth !== fifth && fourth !== sixth){
+        output.innerHTML += " " + fourth; 
+    }
+    if(fifth !== first && fifth !== second && fifth !== third && fifth !== fourth && fifth !== sixth){
+        output.innerHTML += " " + fifth; 
+    }
+    if (sixth !== first && sixth !== second && sixth !== third && sixth !== fourth && sixth !== fifth){
+        output.innerHTML += " " + sixth; 
+    }
+    if (output.innerHTML === "") {
+        output.innerHTML = "There are no unique numbers";
+    }  
 }
 
 
